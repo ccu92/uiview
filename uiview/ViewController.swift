58fe61b9b4afc95e3cc408f1cf1815c71ce42119
ccu92 <yenping.tsao@gmail.com>
2019-03-07 08:48:31 +0800

    deactivate CGRect which create view

--- uiview/ViewController.swift
+++ uiview/ViewController.swift
@@ -13,6 +13,128 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        var viewArea = CGRect(x: 160, y: 60, width: 30, height: 10)
+//        var smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 190, y: 70, width: 20, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 210, y: 80, width: 10, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 220, y: 90, width: 10, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 230, y: 100, width: 10, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 210, y: 110, width: 20, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 180, y: 120, width: 30, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        //
+//        viewArea = CGRect(x: 160, y: 130, width: 20, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//        //
+//
+//        viewArea = CGRect(x: 180, y: 140, width: 30, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 210, y: 150, width: 20, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 230, y: 160, width: 10, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 220, y: 170, width: 10, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 210, y: 180, width: 10, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 190, y: 190, width: 20, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 160, y: 200, width: 30, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 140, y: 190, width: 20, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 130, y: 180, width: 10, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 120, y: 170, width: 10, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 110, y: 150, width: 10, height: 20)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 100, y: 120, width: 10, height: 30)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 110, y: 100, width: 10, height: 20)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 120, y: 90, width: 10, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 130, y: 80, width: 10, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
+//
+//        viewArea = CGRect(x: 140, y: 70, width: 20, height: 10)
+//        smallRect = UIView(frame: viewArea)
+//        smallRect.backgroundColor = UIColor.black
+//        view.addSubview(smallRect)
     }
 
 
